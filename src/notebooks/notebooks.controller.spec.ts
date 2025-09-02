65f322fcf0c974e40354ec29547e68afe3eae412
MarineVega <marinev33@hotmail.com>
2025-09-02 08:28:54 -0300

    volví los archivos como estaban originalmente, tienen un error en es test

--- src/notebooks/notebooks.controller.spec.ts
+++ src/notebooks/notebooks.controller.spec.ts
@@ -1,31 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotebooksController } from './notebooks.controller';
 import { NotebooksService } from './notebooks.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Notebook } from './entities/notebook.entity';
 
 describe('NotebooksController', () => {
   let controller: NotebooksController;
-  let service: NotebooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotebooksController],
-      providers: [
-        NotebooksService,
-        {
-          provide: getRepositoryToken(Notebook),
-          useValue: {
-            find: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-          },
-        },          
-    ],
+      providers: [NotebooksService],
     }).compile();
 
     controller = module.get<NotebooksController>(NotebooksController);
-    service = module.get<NotebooksService>(NotebooksService);
   });
 
   it('should be defined', () => {
--- src/notebooks/notebooks.service.spec.ts
+++ src/notebooks/notebooks.service.spec.ts
@@ -1,12 +1,8 @@
-/*import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { NotebooksService } from './notebooks.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Notebook } from './entities/notebook.entity';
 
 describe('NotebooksService', () => {
   let service: NotebooksService;
-  let repo: Repository</Notebook>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,46 +14,5 @@ describe('NotebooksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-});
-*/
-import { Test, TestingModule } from '@nestjs/testing';
-import { NotebooksService } from './notebooks.service';
-import { getRepositoryToken } from '@nestjs/typeorm';     // Mockeo el repo
-import { Notebook } from './entities/notebook.entity';
-import { Repository } from 'typeorm';
-
-describe('NotebooksService', () => {
-  let service: NotebooksService;
-  let repo: Repository<Notebook>;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        NotebooksService,
-        {
-          provide: getRepositoryToken(Notebook),      // Inyecto el mock en los providers
-          useValue: {
-            find: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-          },
-        },
-      ],
-    }).compile();
-
-    service = module.get<NotebooksService>(NotebooksService);
-    repo = module.get<Repository<Notebook>>(getRepositoryToken(Notebook));
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  it('should return all notebooks', async () => {
-    const notebooks = [{ id: 1, title: 'Test notebook' }] as Notebook[];
-    jest.spyOn(repo, 'find').mockResolvedValueOnce(notebooks);      // Hice un test para findAll()
-
-    expect(await service.findAll()).toEqual(notebooks);
   });
 });
