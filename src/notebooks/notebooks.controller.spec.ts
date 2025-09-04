40846ccf3bfcc8d3f5280848ef97342d6d9b555a
MarineVega <marinev33@hotmail.com>
2025-09-04 11:45:26 -0300

    se realizadon los test de integración

--- src/notebooks/notebooks.controller.spec.ts
+++ src/notebooks/notebooks.controller.spec.ts
@@ -2,8 +2,43 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { NotebooksController } from './notebooks.controller';
 import { NotebooksService } from './notebooks.service';
 import { Notebook } from './entities/notebook.entity';
-import { CreateNotebookDto } from './dto/create-notebook.dto';
+//import { CreateNotebookDto } from './dto/create-notebook.dto';    // para pruebas unitarias
 
+/* Prueba de integración */
+describe('NotebooksController (Integración)', () => {
+  let controller: NotebooksController;
+  let service: NotebooksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [NotebooksController],
+      providers: [NotebooksService],
+    }).compile();
+
+    controller = module.get<NotebooksController>(NotebooksController);
+    service = module.get<NotebooksService>(NotebooksService);
+  });
+
+  it('Devolver las notebooks usando el servicio real', async () => {
+    const notebooks: Notebook[] = await controller.findAll();
+    expect(Array.isArray(notebooks)).toBe(true);
+    expect(notebooks.length).toBeGreaterThanOrEqual(0);
+  });
+
+  it('Crear una notebook usando el servicio real', async () => {
+    const newNotebook = await controller.create({ title: 'Nueva', content: 'Contenido' });
+    expect(newNotebook).toHaveProperty('id');
+    expect(newNotebook.title).toBe('Nueva');
+  });
+
+});
+
+
+
+
+
+/*    Prueba Unitaria */
+/*
 describe('NotebooksController', () => {
   let controller: NotebooksController;
   let service: NotebooksService;      // mokear el service para probar el controlador
@@ -57,4 +92,5 @@ describe('NotebooksController', () => {
   });
   
 });
+*/
 
